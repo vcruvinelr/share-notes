999e8581b04f1cdd37938c7bdc30031a734203a6
Vinicius Cruvinel Rego <vcruvinelr@192.168.68.55>
2025-12-20 18:25:38 -0300

    update backend port

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,4 +8,4 @@ echo "Running database migrations..."
 poetry run alembic upgrade head
 
 echo "Starting application..."
-exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8010 --reload

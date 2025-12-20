df7a9b5b4352dd7739b070831c72d2a92b058783
Vinicius Cruvinel Rego <vcruvinelr@192.168.68.55>
2025-12-20 19:27:38 -0300

    update init.sql

--- database/postgres/init.sql
+++ database/postgres/init.sql
@@ -1,6 +1,5 @@
--- Create Keycloak database if it doesn't exist
-SELECT 'CREATE DATABASE keycloak'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
+-- Create Keycloak database
+CREATE DATABASE keycloak;
 
 -- Note: User 'syncpad' is already created via POSTGRES_USER env var
 -- Database 'syncpad' is already created via POSTGRES_DB env var
